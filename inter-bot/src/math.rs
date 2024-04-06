38a752803462d6146741bbaa9d8b58fb07952263
Karthik Shaji <kshaji3@gatech.edu>
2024-04-06 16:46:43 -0400

    remove whitepsace

--- inter-bot/src/math.rs
+++ inter-bot/src/math.rs
@@ -3,6 +3,7 @@ use serenity::model::channel::Message;
 pub fn pemdas(msg: &Message) -> String {
 
     //clean out the whitespace
-    let post: String = msg.content.chars().skip(10).collect();
+    let mut post: String = msg.content.chars().skip(10).collect();
+    post = post.trim().replace(' ', "").to_string();
     post
 }
