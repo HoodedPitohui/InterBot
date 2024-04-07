f34632e4f89b1edf3bfecb7ec300a1133ba84865
Karthik Shaji <kshaji3@gatech.edu>
2024-04-07 10:03:51 -0400

    updated the trolling message list

--- inter-bot/src/messages.rs
+++ inter-bot/src/messages.rs
@@ -1,6 +1,6 @@
 pub mod troll_messages {
     use serenity::model::channel::Message;
-    use std::rand::{task_rng, Rng};
+    use rand::Rng;
     pub fn hello_message(msg: &Message) -> String {
         let reply = format!("<@{}>, 0/0/0? Lol. And you call yourself \"challenger\"? 
         Don't make me laugh.There's a reason Europe never wanted you. Because you're bad. 
@@ -18,20 +18,32 @@ pub mod troll_messages {
     pub fn king_troll_message(msg: &Message) -> String {
         //return a massively troll message
         let mut post: String = msg.content.chars().skip(4).collect();
-        let mut temp = post.split(" ");
+        let mut temp = post.trim().split(" ");
 
         //get the [preferably name] of the person that is sent to the function
         post = temp.next().unwrap_or("No substring found").to_string();
-        let addition = String::from(" is a ");
+        let addition = String::from("is a");
+
+        //add an intermediate line
+        let intermediate = vec!["total".to_string(), "unadulterated".to_string(), "half".to_string(), "omega".to_string(),
+                                "mega".to_string(), "feeding".to_string()];
 
         //insult list
         let comments = vec!["clown!".to_string(), "idiot!".to_string(), "inter!".to_string(),
                                         "troll!".to_string(), "muppet!".to_string(), "yasuo main!".to_string(),
-                                        "genius!".to_string()];
-        let num: f64 = task_rng().gen_range(0, comments.len()); 
-        let chosen = &comments[num];
+                                        "genius!".to_string(), "pillock!".to_string(), "bugger".to_string(),
+                                        "arsehole".to_string(), "lint licker!".to_string(), "pompous worm!".to_string(),
+                                        "fat lard!".to_string()];
+        
+        //ping a random insult from the insult list
+        let mut rng = rand::thread_rng();
+        let num = rng.gen_range(0..intermediate.len());
+        let chosen_intermediate = &intermediate[num];
+        let num = rng.gen_range(0..comments.len()); 
+        let chosen_insult = &comments[num];
 
-        let ret_string = format!("{} {} {}", post, addition, chosen);
+        //combine the messages
+        let ret_string = format!("{} {} {} {}", post, addition, chosen_intermediate, chosen_insult);
         ret_string
     }
 }
