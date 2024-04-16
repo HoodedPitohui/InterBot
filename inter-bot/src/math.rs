f55ecafbd5985d37128b3f3b33a502d32f9c5f17
Karthik Shaji <kshaji3@gatech.edu>
2024-04-16 14:51:23 -0400

    updated error handling

--- inter-bot/src/math.rs
+++ inter-bot/src/math.rs
@@ -10,6 +10,8 @@ enum Token {
 //order of operations calculator
 pub fn pemdas(msg: &Message) -> String {
 
+    //shunting yard algorithm: converts to Reverse Polish Notation (RPN) and then evaluates it
+
     //clean out the whitespace
     let mut post: String = msg.content.chars().skip(10).collect();
     
@@ -17,38 +19,65 @@ pub fn pemdas(msg: &Message) -> String {
     post = post.trim().to_string();
     let res = parse_expression(&post);
 
+    //standard way of setting up the problem
     let mut output_queue: VecDeque<Token> = VecDeque::new();
+
+    //operators not added to the output queue
     let mut operator_stack: VecDeque<Token> = VecDeque::new();
     let mut iter = 0;
     for element in res {
         match element {
+
+            //add numbers to the back
             Token::Number(_) => output_queue.push_back(element),
-            Token::Operator(temp_element) => {
+            Token::Operator(temp_element) => { //this operator is O2
                 while let Some(Token::Operator(top_element)) = operator_stack.back() {
+
+                    //check precedence and associativity following the rule for O2 and O1
+                    //there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
+                    //and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
+
                     if get_precedence(*top_element) > get_precedence(temp_element) ||
                        (get_precedence(*top_element) == get_precedence(temp_element) && !get_associativity(temp_element)) {
+
+                        //pop o2 from the operator stack into the output queue
                         output_queue.push_back(operator_stack.pop_back().unwrap());
                     } else {
                         break;
+                        //end condition reached
                     }
                 }
+
+                //push O1 onto the operator stack
                 operator_stack.push_back(element);
             },
             Token::LeftParen => operator_stack.push_back(element),
             Token::RightParen => {
+
+                //goal: find the associative left parenthesis
+                let mut found_paren = false;
                 while let Some(top) = operator_stack.pop_back() {
                     if let Token::LeftParen = top {
+                        found_paren = true;
                         break;
                     } else {
+                        //pop the operator from the operator stack into the output queue
                         output_queue.push_back(top);
                     }
+                }
+                if !found_paren {
+                    return "Mismatched parenthesis".to_string();
                 }
             }
         }
     }
     
+    //make sure that the operator on the top of the stack is not a left parenthesis
     while let Some(element) = operator_stack.pop_back() {
-        output_queue.push_back(element);
+        match element {
+            Token::LeftParen => return "Mismatched parenthesis".to_string(),
+            _ => output_queue.push_back(element),
+        }
     }
 
     let mut eval_stack: VecDeque<f64> = VecDeque::new();
@@ -56,26 +85,30 @@ pub fn pemdas(msg: &Message) -> String {
         match element {
             Token::Number(n) => eval_stack.push_back(n),
             Token::Operator(element) => {
-                let right = eval_stack.pop_back().expect("Wrong syntax");
-                let left = eval_stack.pop_back().expect("Wrong syntax");
+
+                //this means there are too many operators
+                if eval_stack.len() < 2 {
+                    return "Invalid expression, not enough operands".to_string();
+                }
+                let right = eval_stack.pop_back().unwrap();
+                let left = eval_stack.pop_back().unwrap();
                 let result = match element {
                     '+' => left + right,
                     '-' => left - right,
                     '*' => left * right,
                     '/' => left / right,
                     '^' => left.powf(right),
-                    _ => 0.0,
+                    _ => return "Invalid Expression".to_string(),
                 };
                 eval_stack.push_back(result);
             },
             _ => (),
         }
     }
-    // eval_stack.pop_back().expect("Invalid expression");
-
-    //there is probably an issue with this line of code
-    // return String::from(format!("length of eval stack: {}", eval_stack.len()));
-    let final_result = String::from(eval_stack.pop_back().expect("Invalid expression").to_string());
+    let final_result: String = match eval_stack.pop_back() {
+        Some(value) => value.to_string(),
+        None => "Invalid expression, no result computed".to_string(),
+    };
     final_result
     // final_result
 }
